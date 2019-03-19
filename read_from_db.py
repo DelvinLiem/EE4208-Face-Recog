c9cd2236527c567f29bb1ef183d3d100abfe0233
DelvinLiem <dlvn.liem@gmail.com>
2019-03-19 15:33:49 +0800

    added comments

--- read_from_db.py
+++ read_from_db.py
@@ -5,14 +5,13 @@ import os
 
 main_db = "./database"
 
-def read_from_db (database):
+def read_from_db(database):
 
     entries = [person for person in os.listdir(database)]
 
     entries.sort()
 
     image_list = []
-    image_list = np.array(image_list)
     index =[]
 
     for count, name in enumerate(entries):
@@ -21,22 +20,26 @@ def read_from_db (database):
 
         for face in os.listdir(folder_path):
             
+            # image index listing
             index.append(face.split('.')[0])
 
+            # read image in grayscale
             image = cv2.imread(folder_path+ '/' + face, 0)
             
+            # retrieve exactly 10000 pixels
             image = image.flatten()
-            
             image = np.reshape(image, (1, len(image)))
             image = image[:10000]
             
+            # populate the image list
             if len(image_list) < 1:
                 image_list = image
             else:
                 image_list = np.append(image_list, image, axis=0)
                
+    # save all the image indexes 
+    np.savetxt("name_index.csv", index, delimiter='  ,  ', fmt = '%s')
 
-    np.savetxt("person_index.csv", index, delimiter='  ,  ', fmt = '%s')
     return image_list
 
 print(read_from_db(main_db))
